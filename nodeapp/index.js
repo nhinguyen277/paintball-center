581c69915cacf61a5a9ba7bf1a015e486f09bc13
Nhi Ha Thao Nguyen <n01593039@humber.ca>
2024-06-17 03:44:00 -0400

    Update index.js

--- nodeapp/index.js
+++ nodeapp/index.js
@@ -29,7 +29,7 @@ app.use(express.json()); //need this line to be able to receive/parse JSON from 
 //allow requests from all servers
 app.use(
   cors({
-    origin: ["https://paintball-center.vercel.app/"],
+    origin: ["https://paintball-center.vercel.app"],
     methods: ["GET","PUT","POST","DELETE"],
     credentials: true,
   })
